98fd512ffba2e4d673df771a8c80d85fc2518ecb
Vadim Migunov <vadim.migunov@gmail.com>
2021-12-30 00:59:04 +0300

    Freeze credentials file 2

--- source/src/connection_credentials.h
+++ source/src/connection_credentials.h
@@ -1,14 +1,13 @@
 #include <Arduino.h>
-
+ 
 struct { 
     char ssid[20] = "your_WiFi_SSID";
     char pass[20] = "your_WIFI_pass";
-  } WIFI_DATA;
-
+} WIFI_DATA;
+ 
 struct { 
     uint16 port = 1234; // PUT MQTT SERVER PORT NUMBER HERE
     char pass[24] = "your_MQTT_pass";
     char user[16] = "your_MQTT_user";
     uint8 ip[4] = {256,256,256,256}; // PUT MQTT SERVER IP ADDRESS HERE
-  } MQTT_DATA;
-  
+} MQTT_DATA;
